720537cc826ad630983feb6b4d802fd701a52013
Patrycja Hyjek <patrycja.hyjek@gmail.com>
2015-08-21 12:16:48 +0200

    price2

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150820121143) do
+ActiveRecord::Schema.define(version: 20150821082629) do
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20150820121143) do
     t.datetime "updated_at",                          null: false
     t.string   "category"
     t.string   "avatar"
+    t.decimal  "price2"
   end
 
   create_table "users", force: :cascade do |t|
